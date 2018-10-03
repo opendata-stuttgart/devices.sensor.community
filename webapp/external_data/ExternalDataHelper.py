a4a189abeb0fc8d8769c75535f9cd9c43bd1758d
David Lackovic <david.lackovic@me.com>
2018-10-03 11:20:29 +0200

    changed copyright, impressum and privacy page

--- webapp/external_data/ExternalDataHelper.py
+++ webapp/external_data/ExternalDataHelper.py
@@ -1,7 +1,8 @@
 # encoding: utf-8
 
 """
-Copyright (c) 2017, Ernesto Ruge
+Copyright (c) 2018, Maintainer: David Lackovic
+based on Ernesto Ruge https://github.com/ruhrmobil-E/meine-luftdaten/
 All rights reserved.
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
