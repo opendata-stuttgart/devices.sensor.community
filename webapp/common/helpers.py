f1a52f402bc4177f48b48ddcd19eafffd3b77056
ohheyitsdave <david.lackovic@me.com>
2019-02-23 23:21:44 +0100

    added inbox checkbox requiredIf validator for sensor registration form set additional information as optional

--- webapp/common/helpers.py
+++ webapp/common/helpers.py
@@ -17,6 +17,7 @@ from flask.json import JSONEncoder as BaseJSONEncoder
 from sqlalchemy import inspect
 from sqlalchemy.orm import exc
 from werkzeug.exceptions import abort
+from wtforms.validators import InputRequired, Optional
 
 
 def get_current_time():
@@ -96,3 +97,29 @@ def get_object_or_404(model, *criterion):
         return model.query.filter(*criterion).one()
     except (exc.NoResultFound, exc.MultipleResultsFound):
         abort(404)
+
+
+class RequiredIf(InputRequired):
+    """Validator which makes a field required if another field is set and has a truthy value.
+
+    Sources:
+        - http://wtforms.simplecodes.com/docs/1.0.1/validators.html
+        - http://stackoverflow.com/questions/8463209/how-to-make-a-field-conditionally-optional-in-wtforms
+        - https://gist.github.com/devxoul/7638142#file-wtf_required_if-py
+    """
+    field_flags = ('requiredif',)
+
+    def __init__(self, message=None, *args, **kwargs):
+        super(RequiredIf).__init__()
+        self.message = message
+        self.conditions = kwargs
+
+    # field is requiring that name field in the form is data value in the form
+    def __call__(self, form, field):
+        for name, data in self.conditions.items():
+            other_field = form[name]
+            if other_field is None:
+                raise Exception('no field named "%s" in form' % name)
+            if other_field.data == data and not field.data:
+                InputRequired.__call__(self, form, field)
+            Optional()(form, field)
