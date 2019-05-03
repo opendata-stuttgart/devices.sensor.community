c7b6f9b0915a3a68e38b6dce8335f85f63f6f73c
ricki-z <ricki_z@gmx.de>
2019-05-03 21:27:56 +0000

    Quick fix for countries like "Iran, Islamic Republic of"

--- webapp/common/countrycodes.py
+++ webapp/common/countrycodes.py
@@ -16,5 +16,8 @@ from flask_babelex import Domain
 
 domain = Domain(pycountry.LOCALES_DIR, domain='iso3166')
 
-country_codes = [(c.alpha_2, domain.lazy_gettext(c.name)) for c in pycountry.countries]
+"""
+country_codes = [(c.alpha_2, domain.lazy_gettext(' '.join(c.name.split(',')[::-1]))) for c in pycountry.countries]
+"""
+country_codes = [(c.alpha_2, domain.lazy_gettext(c.name.replace(","," - "))) for c in pycountry.countries]
 country_codes.insert(0,('--','--'))
